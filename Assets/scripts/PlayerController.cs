b36aafed61887d87dca4a4c17305a7398a4fa497
gabriellgomez-creator <gabriell.gomez@discoverystudent.org>
2025-12-11 11:38:41 -0500

    almost done

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+
+    private int count;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void OnTriggerEnter(Collider other) 
+   {
+    if(other.gameObject.CompareTag("PickUp")){
+        other.gameObject.SetActive(false);
+        count++;
+        SetCountText();
+    }
+   }
+   void SetCountText(){
+    countText.text = "Count:" + count.ToString();
+   }
+}
+    
